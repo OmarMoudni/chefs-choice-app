e2cb23bdff5ddde686fff03ee97c19683cafaf8b
OmarMoudni <moudniomar2@gmail.com>
2025-06-17 11:50:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,12 @@
-import streamlit as st
+col1, col2 = st.columns([1, 1.2])
 
-# Configuration de la page
-st.set_page_config(page_title="SaveurMagique", layout="wide")
+with col1:
+    st.image("https://raw.githubusercontent.com/OmarMoudni/chefs-choice-app/main/assets/saveur_header.png", use_column_width=True)
 
-# ---------- Barre de navigation ----------
-st.markdown("""
-    <style>
-        .topnav {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            background-color: white;
-            padding: 15px 30px;
-            font-family: 'Segoe UI', sans-serif;
-            font-size: 17px;
-        }
-        .topnav .left, .topnav .right {
-            display: flex;
-            align-items: center;
-            gap: 25px;
-        }
-        .topnav a {
-            text-decoration: none;
-            color: black;
-            font-weight: 500;
-        }
-        .search-box input {
-            padding: 5px 12px;
-            border-radius: 20px;
-            border: 1px solid #ccc;
-        }
-    </style>
-    <div class="topnav">
-        <div class="left">
-            <img src="https://via.placeholder.com/100x40?text=SaveurMagique" alt="logo">
-            <a href="#">Accueil</a>
-            <a href="#">Menu</a>
-            <a href="#">Recettes</a>
-            <a href="#">Nous contacter</a>
+with col2:
+    st.markdown("""
+        <div style='margin-top:100px;'>
+            <h1 style='font-size:50px; color:#2F2F2F;'>DES RECETTES<br>QUE POUR VOUS</h1>
+            <p style='color:#666;'>RENDEZ-VOUS SUR L’APPLICATION MOBILE 📱</p>
         </div>
-        <div class="right">
-            <div class="se
+    """, unsafe_allow_html=True)
